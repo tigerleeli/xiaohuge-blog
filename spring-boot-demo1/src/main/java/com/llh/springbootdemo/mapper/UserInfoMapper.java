c8b2a57ffb399a6fc9707db173d3e1b79c2f4d8a
刘立虎 <you@example.com>
2021-05-12 13:48:30 +0800

    修改目录结构

--- spring-boot-demo1/src/main/java/com/llh/springbootdemo/mapper/UserInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.llh.springbootdemo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.llh.springbootdemo.entity.UserInfo;
-
-public interface UserInfoMapper extends BaseMapper<UserInfo> {
-}
--- spring-boot-demo1/src/main/java/com/llh/springbootdemo/service/UserInfoService.java
+++
@@ -1,7 +0,0 @@
-package com.llh.springbootdemo.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.llh.springbootdemo.entity.UserInfo;
-
-public interface UserInfoService extends IService<UserInfo> {
-}
