ace66105fe0d0adbc01a5dd8f0fdae23b9142d28
刘立虎 <you@example.com>
2021-05-20 11:50:32 +0800

    Open Feign

---
+++ spring-cloud-alibaba-feign/product-api/src/main/java/com/llh/product/api/ProductApiApplication.java
@@ -0,0 +1,16 @@
+package com.llh.product.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 小虎哥的技术博客
+ */
+@SpringBootApplication
+public class ProductApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApiApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-alibaba-feign/product/src/main/java/com/llh/product/ProductApplication.java
@@ -0,0 +1,16 @@
+package com.llh.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 小虎哥的技术博客
+ */
+@SpringBootApplication
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+}
