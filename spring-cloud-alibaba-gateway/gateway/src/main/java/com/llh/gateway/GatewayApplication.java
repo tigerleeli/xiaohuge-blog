f581d4060e4eefd738ce999cb82ff6d10504b85d
刘立虎 <you@example.com>
2021-05-21 11:33:59 +0800

    gateway

--- spring-cloud-alibaba-gateway/gateway/src/main/java/com/llh/gateway/GatewayApplication.java
+++ spring-cloud-alibaba-gateway/gateway/src/main/java/com/llh/gateway/GatewayApplication.java
@@ -2,13 +2,11 @@ package com.llh.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 小虎哥的技术博客
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
