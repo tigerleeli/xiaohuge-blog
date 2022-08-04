e906639e0c93be2555c2515b367b11d35a65a6ab
刘立虎 <1286318172@qq.com>
2022-08-04 10:23:44 +0800

    博客

---
+++ java-primary-study/src/main/java/lesson15/Demo.java
@@ -0,0 +1,13 @@
+package lesson15;
+
+public class Demo {
+    public static void main(String[] args) {
+        try {
+            // 代码
+        } catch (Exception e) {
+            // 处理异常
+        } finally {
+            //
+        }
+    }
+}
