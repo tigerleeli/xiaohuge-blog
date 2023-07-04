e922fb7ecdff87a2a620ec7d43488a268a7ea953
刘立虎 <1286318172@qq.com>
2023-07-04 17:42:21 +0800

    money keep

---
+++ money-keep/money-keep/src/main/java/com/llh/moneykeep/MoneyKeepApplication.java
@@ -0,0 +1,13 @@
+package com.llh.moneykeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyKeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyKeepApplication.class, args);
+    }
+
+}
