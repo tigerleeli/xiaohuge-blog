38f829728f84d499dec0b03431fc931e569ca861
刘立虎 <1286318172@qq.com>
2023-08-28 15:42:54 +0800

    sentinel

---
+++ sourcecode/spring-cloud-demo/sentinel-demo/src/main/java/com/example/sentineldemo/controller/SentinelController.java
@@ -0,0 +1,17 @@
+package com.example.sentineldemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("sentinel")
+public class SentinelController {
+    @GetMapping("test")
+    public String test() {
+        System.out.println(LocalDateTime.now());
+        return "hello world";
+    }
+}
