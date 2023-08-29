16a3668f1da8cb6fa59b5c762dfbaa930fb55c67
刘立虎 <1286318172@qq.com>
2023-08-29 15:59:43 +0800

    分库分表

---
+++ sourcecode/spring-cloud-demo/sharding-jdbc-demo/src/main/java/com/example/shardingjdbcdemo/ShardingJdbcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.shardingjdbcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJdbcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJdbcDemoApplication.class, args);
+    }
+
+}
