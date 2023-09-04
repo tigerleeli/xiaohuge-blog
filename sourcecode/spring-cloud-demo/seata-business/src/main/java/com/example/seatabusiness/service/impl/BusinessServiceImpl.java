1a0e07f6e343e443a03ae83fb984f2c568cc60b2
15761632047 <1286318172@qq.com>
2023-09-04 21:33:46 +0800

    分布式事务

--- sourcecode/spring-cloud-demo/seata-business/src/main/java/com/example/seatabusiness/service/impl/BusinessServiceImpl.java
+++ sourcecode/spring-cloud-demo/seata-business/src/main/java/com/example/seatabusiness/service/impl/BusinessServiceImpl.java
@@ -23,7 +23,7 @@ public class BusinessServiceImpl implements BusinessService {
         // 调用订单服务创建订单
         orderService.createOrder(1L, 1L, 1, 1499);
         // 模拟异常
-        int i = 1 / 0 ;
+        // int i = 1 / 0 ;
         // 调用商品服务扣除库存
         productService.decreaseNumber(1L, 1);
     }
