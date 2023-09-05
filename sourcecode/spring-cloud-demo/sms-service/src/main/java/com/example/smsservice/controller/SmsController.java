d3a573967990a061633a1577eb17cf27d649b3aa
刘立虎 <1286318172@qq.com>
2023-09-05 14:42:09 +0800

    发送短信

---
+++ sourcecode/spring-cloud-demo/sms-service/src/main/java/com/example/smsservice/controller/SmsController.java
@@ -0,0 +1,21 @@
+﻿package controller;
+
+import com.example.smsservice.service.SmsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("sms")
+public class SmsController {
+    @Resource
+    private SmsService smsService;
+
+    @GetMapping("sendSmsVerifyCode")
+    public void sendSmsVerifyCode(@RequestParam String phone) {
+        smsService.sendSmsVerifyCode(phone);
+    }
+}
